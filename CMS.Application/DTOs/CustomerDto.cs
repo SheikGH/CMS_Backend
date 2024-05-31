484f6fbcfedebb9aeb3d6b7a1cd7b485ec0b24c7
SheikGH <sheik.ak2006@gmail.com>
2024-05-31 15:00:29 +0400

    Testing Update

--- CMS.Application/DTOs/CustomerDto.cs
+++ CMS.Application/DTOs/CustomerDto.cs
@@ -31,5 +31,7 @@ namespace CMS.Application.DTOs
         public string Phone { get; set; }
         [Required]
         public string Address { get; set; }
+        public string Password { get; set; }
+        public string PasswordKey { get; set; }
     }
 }
